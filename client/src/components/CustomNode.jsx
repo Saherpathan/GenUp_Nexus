904f0a88ffd8fed35dc6fade976e7b8beebc756f
Siddhant Patil <shimpli.patil2804@gmail.com>
2024-03-27 05:46:42 +0530

    Update axios URL and import CustomNode component

--- client/src/components/CustomNode.jsx
+++ client/src/components/CustomNode.jsx
@@ -1,74 +1,34 @@
-import React, { memo } from 'react';
-import { Handle, useReactFlow, useStoreApi, Position } from 'reactflow';
+import { Button } from "@nextui-org/react";
+import React, { memo } from "react";
+import { Handle, useReactFlow, useStoreApi, Position } from "reactflow";
 
-const options = [
-  {
-    value: 'smoothstep',
-    label: 'Smoothstep',
-  },
-  {
-    value: 'step',
-    label: 'Step',
-  },
-  {
-    value: 'default',
-    label: 'Bezier (default)',
-  },
-  {
-    value: 'straight',
-    label: 'Straight',
-  },
-];
+function CustomNode({ data, isConnectable, id }) {
+  function handleClick() {
+    console.log("clicked");
+  }
 
-function Select({ value, handleId, nodeId }) {
-  const { setNodes } = useReactFlow();
-  const store = useStoreApi();
-
-  const onChange = (evt) => {
-    const { nodeInternals } = store.getState();
-    setNodes(
-      Array.from(nodeInternals.values()).map((node) => {
-        if (node.id === nodeId) {
-          node.data = {
-            ...node.data,
-            selects: {
-              ...node.data.selects,
-              [handleId]: evt.target.value,
-            },
-          };
-        }
-
-        return node;
-      })
-    );
-  };
-
-  return (
-    <div className="custom-node__select">
-      <div>Edge Type</div>
-      <select className="nodrag" onChange={onChange} value={value}>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
-      <Handle type="source" position={Position.Right} id={handleId} />
-    </div>
-  );
-}
-
-function CustomNode({ id, data }) {
   return (
     <>
-      <div className="custom-node__header">
-        This is a <strong>custom node</strong>
+      {/* <Handle
+        type="target"
+        position={Position.Top}
+        isConnectable={isConnectable}
+      /> */}
+      <div className="text-md">
+        <div className="text-xl text-yellow-700">{data.label}</div>
+        <div className="">
+          {data.description}
+          {data.icon}
+        </div>
+        <Button onClick={handleClick}>Details</Button>
       </div>
-      <div className="custom-node__body">
-        {Object.keys(data.selects).map((handleId) => (
-          <Select key={handleId} nodeId={id} value={data.selects[handleId]} handleId={handleId} />
-        ))}
-      </div>
+
+      {/* <Handle
+        type="source"
+        position={Position.Right}
+        id=""
+        isConnectable={isConnectable}
+      /> */}
     </>
   );
 }
