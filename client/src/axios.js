74808e7c7c622a9fd3f4644ac8bf2bc14a70f004
Siddhant Patil <shimpli.patil2804@gmail.com>
2024-03-28 06:52:30 +0530

    linked with deployed server

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 const debug = localStorage.getItem("debug");
 
-const url = "http://192.168.29.59:5000";
+const url = "https://gen-up-nexus-hhs3.vercel.app"; 
+//! For using local Development, use this URL to connect to your local server.
 
 console.log(url);
 
 const instance = axios.create({
-  baseURL: url, //! For using local Development, use this URL to connect to your local server.
+  baseURL: url,
 });
 
 instance.interceptors.request.use((req) => {
